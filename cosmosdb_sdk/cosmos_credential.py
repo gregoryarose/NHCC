bf4ff91ab7c6b12f5a8c2f95942b079d0d3f4f9e
gregoryarose <90962237+gregoryarose@users.noreply.github.com>
2022-02-15 14:16:16 +1100

    Add files via upload

---
+++ cosmosdb_sdk/cosmos_credential.py
@@ -0,0 +1,7 @@
+import os
+
+# Cosmos DB config
+cosmosdb_config = {
+    'ENDPOINT': 'https://nhcc.documents.azure.com:443/',
+    'PRIMARYKEY': 'l3xURYYGuxKbe7NqFdYKX3RaGItUBuHJ9yffeXRpdP7eLBvtyufxSPShBaCwWlLKmkRbUIvdaGL6xB9i9QNqzw=='
+}
