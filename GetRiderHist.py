adcfe237825b7dd47236e4f50e2bf1c906a44d2c
gregoryarose <gregory.rose@optusnet.com.au>
2022-02-20 13:31:27 +1100

    update to points expire calculation

--- GetRiderHist.py
+++ GetRiderHist.py
@@ -5,6 +5,10 @@ import json
 import numpy as np
 import pandas as pd
 import sys
+from datetime import datetime
+from datetime import timedelta
+
+
 pd.set_option('display.colheader_justify', 'center')
 
 cosmosDBName = 'Grading'
@@ -13,9 +17,14 @@ dbConnection = CosmosDB(cosmosDBName, containerName)
  
 def GetHist(License):
 
+    PointsExpire = 183
+    FreezeDate = "2022-04-27"
+    if datetime.today().strftime('%Y-%m-%d') < FreezeDate:
+        PointsExpire +=85
+
 
     query = 'SELECT c.FirstName, c.Surname,c.Grade,t.RaceGrade,t.RaceDate,t.RaceResult ?? "-" as RaceResult ,t.RacePoints ?? "" as Points FROM c JOIN t IN c.RaceEntry WHERE c.License = ' + str(License)  
-    print(query)
+    #print(query)
     try:
         results = dbConnection.query_item(query)
         df = pd.DataFrame(results, columns=None)
@@ -30,7 +39,7 @@ def GetHist(License):
     sorted_df = df2.sort_values(by='RaceDate', ascending=False)
     left_aligned_df = df.style.set_properties(**{'text-align': 'left'})
     
-    query2 = 'SELECT VALUE root FROM (SELECT sum(t.RacePoints) as Active_Points FROM c JOIN t IN c.RaceEntry WHERE  t.RaceGrade <= c.Grade  and DateTimeDiff("day", t.RaceDate, GetCurrentDateTime ()) <= 269 and c.License = ' + str(License)+ ' )as root ' 
+    query2 = 'SELECT VALUE root FROM (SELECT sum(t.RacePoints) as Active_Points FROM c JOIN t IN c.RaceEntry WHERE  t.RaceGrade <= c.Grade  and DateTimeDiff("day", t.RaceDate, GetCurrentDateTime ()) <= ' + str(PointsExpire) + ' and c.License = ' + str(License)+ ' )as root ' 
     #print(query2)
     try:
         results2 = dbConnection.query_item(query2)
@@ -62,9 +71,14 @@ def GetRider(Nm):
   
     
 def GetGrades():
+
+    PointsExpire = 183
+    FreezeDate = "2022-04-27"
+    if datetime.today().strftime('%Y-%m-%d') < FreezeDate:
+        PointsExpire +=85
  
     query = 'SELECT VALUE root FROM (SELECT c.Grade, UPPER(c.Surname) as Surname, c.FirstName,c.club, '
-    query += 'sum((t.RaceGrade <= c.Grade and DateTimeDiff("day", t.RaceDate, GetCurrentDateTime ()) <= 269)? t.RacePoints:0) as Points FROM c  JOIN t IN c.RaceEntry '
+    query += 'sum((t.RaceGrade <= c.Grade and DateTimeDiff("day", t.RaceDate, GetCurrentDateTime ()) <= ' + str(PointsExpire) +' )? t.RacePoints:0) as Points FROM c  JOIN t IN c.RaceEntry '
     query += 'Group By c.Grade, c.Surname, c.FirstName,c.club)as root ' 
     
     try:
@@ -78,9 +92,16 @@ def GetGrades():
     return sorted_df
 
 def GetTopPoints():
+    PointsExpire = 183
+    FreezeDate = "2022-04-27"
+    if datetime.today().strftime('%Y-%m-%d') < FreezeDate:
+        PointsExpire +=85
+
+# point expirey is usally 6 months - due to covid break extra days allowed till this date
+
  
     query = 'SELECT VALUE root FROM (SELECT c.Grade, c.Surname, c.FirstName,c.club, '
-    query += 'sum((t.RaceGrade <= c.Grade and DateTimeDiff("day", t.RaceDate, GetCurrentDateTime ()) <= 269)? t.RacePoints:0) as Points FROM c  JOIN t IN c.RaceEntry '
+    query += 'sum((t.RaceGrade <= c.Grade and DateTimeDiff("day", t.RaceDate, GetCurrentDateTime ()) <= ' + str(PointsExpire) +' )? t.RacePoints:0) as Points FROM c  JOIN t IN c.RaceEntry '
     query += 'Group By c.Grade, c.Surname, c.FirstName,c.club)as root where root.Points >= 8 ' 
     
     try:
@@ -123,4 +144,4 @@ def GetRaceResults(RID):
     return sorted_df   
 
 
- 
+
